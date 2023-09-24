1b5893e0f21651bb462f52d1a5222601684aa945
Kevin Liu <kevinxo328@gmail.com>
2023-09-24 13:51:48 +0800

    Remove: useless avatar image

--- src/components/chatroom/chat-message.tsx
+++ src/components/chatroom/chat-message.tsx
@@ -1,5 +1,5 @@
 import { Chat } from "./chatroom";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 type Props = Chat;
 
