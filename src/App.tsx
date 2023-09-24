c9758900ca4986b4754c3201020729ba0d515ecc
Kevin Liu <kevinxo328@gmail.com>
2023-09-24 20:10:26 +0800

    Test: remove app test and remove demo api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
-
-import { useGetUser } from "./apis/api";
 import AppLayout from "./components/app/app-layout";
 import AppAside from "./components/app/app-aside";
 import { Outlet, useLocation } from "react-router-dom";
 import ThemeToggle from "@/components/theme-toggle";
 
 function App() {
-  const { data, error } = useGetUser();
   const location = useLocation();
 
   const [asideItems, setAsideItems] = useState([
@@ -25,8 +22,6 @@ function App() {
       pre.map((item) => ({ ...item, active: item.path === location.pathname }))
     );
   }, [location]);
-
-  console.log(data, error);
 
   const aside = (
     <div className="p-4">
