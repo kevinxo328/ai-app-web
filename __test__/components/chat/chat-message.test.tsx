1a234572a572f9e2e7cb292f177f62e9eebe2d20
Kevin Liu <kevinxo328@gmail.com>
2023-09-24 17:07:10 +0800

    Test: add chat message test

---
+++ __test__/components/chat/chat-message.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import ChatMessage from "@/components/chatroom/chat-message";
+import { Chat } from "@/components/chatroom/chatroom";
+
+const props: Chat = {
+  role: "ai",
+  message: "TestMessage",
+};
+
+describe("ChatMessage", () => {
+  it("render role", () => {
+    render(<ChatMessage role={props.role} message={props.message} />);
+
+    const role = screen.getByText(props.role.toUpperCase());
+    expect(role).toBeInTheDocument();
+  });
+
+  it("render message", () => {
+    render(<ChatMessage role={props.role} message={props.message} />);
+
+    const message = screen.getByText(props.message);
+    expect(message).toBeInTheDocument();
+  });
+});
