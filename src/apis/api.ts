a00f5e84ad20753ab0bc93540f1a4fa8119ba6de
Kevin Liu <kevinxo328@gmail.com>
2023-09-29 11:57:38 +0800

    Refactor: fix misspelling

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,10 +1,10 @@
 import { useMutation, useQuery } from "react-query";
-import apiCliet from "@/lib/apiCliet";
+import apiClient from "@/lib/apiClient.ts";
 import { MutationOptions, ResOpenAI } from "@/types/api";
 
 export function useGetUser() {
   return useQuery("/user", async () => {
-    return await apiCliet.get("/user");
+    return await apiClient.get("/user");
   });
 }
 
@@ -12,6 +12,6 @@ export function usePostOpenAI(options?: MutationOptions<ResOpenAI>) {
   return useMutation(async (text: string) => {
     const query = new URLSearchParams();
     query.append("text", text);
-    return await apiCliet.post(`/openai?${query.toString()}`);
+    return await apiClient.post(`/openai?${query.toString()}`);
   }, options);
 }
